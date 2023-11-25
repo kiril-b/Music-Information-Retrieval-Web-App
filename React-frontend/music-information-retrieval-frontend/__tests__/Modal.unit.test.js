78d1089edc9bffa8d7d4c2d85617ae1ea955c63d
Elena Keckaroska <keckaroska.elena@gmail.com>
2023-11-25 23:03:15 +0100

    [test] Add Modal-details unit test

---
+++ React-frontend/music-information-retrieval-frontend/__tests__/Modal.unit.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Modal from '../src/components/Details/Modal';
+
+const fs = require('fs');
+describe('Modal component', () => {
+  test('renders modal with track details and play button', async () => {
+    const details = {
+      "db_id": 2,
+      "track_id": 5,
+      "track_path": "./src/audio/Kid Bloom Cowboy Official Visualizer.mp3",
+      "track_title": "This World",
+      "artist_name": "AWOL",
+      "track_duration": 206,
+      "track_genre": "Hip-Hop",
+      "track_listens": 1151
+    };
+    const onClose = jest.fn();
+
+    const { getByText, getByTestId } = render(<Modal details={details} onClose={onClose} />);
+
+    expect(getByText('This World')).toBeInTheDocument();
+    expect(getByText('Artist: AWOL')).toBeInTheDocument();
+    expect(getByText('Duration: 206 seconds')).toBeInTheDocument();
+    expect(getByText('Genre: Hip-Hop')).toBeInTheDocument();
+    expect(getByText('Listens: 1151')).toBeInTheDocument();
+
+    const playButton = getByTestId('play-btn');
+    expect(playButton).toBeInTheDocument();
+    global.fetch = jest.fn().mockResolvedValueOnce({
+        status: 200,
+        blob: () => {
+          const mp3FileContent = fs.readFileSync('FastAPI-backend\src\audio\Kid Bloom Cowboy Official Visualizer.mp3');
+      
+          return Promise.resolve(new Blob([mp3FileContent], { type: 'audio/mpeg' }));
+        },
+      });
+    // Click the play button
+    fireEvent.click(playButton);
+
+    await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+  });
+});
