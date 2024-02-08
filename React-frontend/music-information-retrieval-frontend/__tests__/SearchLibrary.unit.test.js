8960bd105b6edb82c3c47cd1e164476a87ef4930
Elena Keckaroska <keckaroska.elena@gmail.com>
2024-02-08 23:35:38 +0100

    [feat] Minor changes in SearchLibrary tests

--- React-frontend/music-information-retrieval-frontend/__tests__/SearchLibrary.unit.test.js
+++ React-frontend/music-information-retrieval-frontend/__tests__/SearchLibrary.unit.test.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { render, screen, fireEvent, act } from '@testing-library/react';
-import SearchLibrary from '../src/components/SearchLibrary/SearchLibrary';
-import '@testing-library/jest-dom/extend-expect';
-import { log } from 'console';
+import "@testing-library/jest-dom/extend-expect";
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import SearchLibrary from "../src/components/SearchLibrary/SearchLibrary";
 
-const fs = require('fs');
+const fs = require("fs");
 const tracks = JSON.parse(
-  fs.readFileSync('./__tests__/response_data/get_tracks.json', 'utf8'),
+  fs.readFileSync("./__tests__/response_data/get_tracks.json", "utf8")
 );
 const tracks_pagination = JSON.parse(
-  fs.readFileSync('./__tests__/response_data/get_tracks_pagination.json', 'utf8'),
+  fs.readFileSync(
+    "./__tests__/response_data/get_tracks_pagination.json",
+    "utf8"
+  )
 );
 
 const track_listens_lower_upper_bound_genre = JSON.parse(
-  fs.readFileSync('./__tests__/response_data/get_tracks_by_searchlibrary_filter.json', 'utf8'),
+  fs.readFileSync(
+    "./__tests__/response_data/get_tracks_by_searchlibrary_filter.json",
+    "utf8"
+  )
 );
 
 const handleFormSubmitMock = {
@@ -22,7 +27,7 @@ const handleFormSubmitMock = {
   },
 };
 
-describe('SearchLibrary Component', () => {
+describe("SearchLibrary Component", () => {
   beforeEach(() => {
     global.fetch = jest.fn().mockResolvedValue({
       json: () => Promise.resolve([]),
@@ -31,16 +36,15 @@ describe('SearchLibrary Component', () => {
     localStorage.clear();
   });
 
-  test('renders the component with loading state', () => {
-
+  test("renders the component with loading state", () => {
     act(() => {
       render(<SearchLibrary />);
     });
 
-    expect(screen.getByText('Loading...')).toBeInTheDocument();
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
   });
 
-  test('renders the component with data', async () => {
+  test("renders the component with data", async () => {
     const mockData = tracks;
 
     global.fetch.mockResolvedValueOnce({
@@ -55,29 +59,28 @@ describe('SearchLibrary Component', () => {
       await new Promise((resolve) => setTimeout(resolve, 0));
     });
 
-    expect(screen.queryByText('Loading...')).toBeNull();
-    expect(screen.getByText('Food - AWOL')).toBeInTheDocument();
+    expect(screen.queryByText("Loading...")).toBeNull();
+    expect(screen.getByText("Food - AWOL")).toBeInTheDocument();
 
-    const getSimilarButtons = screen.getAllByText('Get Similar Tracks');
+    const getSimilarButtons = screen.getAllByText("tt Similar Tracks");
     expect(getSimilarButtons.length).toBe(15);
 
-    const getDetailsButtons = screen.getAllByText('See Details');
+    const getDetailsButtons = screen.getAllByText("See Details");
     expect(getDetailsButtons.length).toBe(15);
 
-    const plusButtons = screen.getAllByText('+');
+    const plusButtons = screen.getAllByText("+");
     expect(plusButtons.length).toBe(15);
-
   });
 
-  test('renders form search with its default values', async () => {
+  test("renders form search with its default values", async () => {
     act(() => {
       render(<SearchLibrary />);
     });
 
-    const lowerBoundInput = screen.getByLabelText('Lower Bound:');
-    const upperBoundInput = screen.getByLabelText('Upper Bound:');
-    const genreSelect = screen.getByLabelText('Genre:');
-    const searchButton = screen.getByText('Search');
+    const lowerBoundInput = screen.getByLabelText("Lower Bound:");
+    const upperBoundInput = screen.getByLabelText("Upper Bound:");
+    const genreSelect = screen.getByLabelText("Genre:");
+    const searchButton = screen.getByText("Search");
 
     expect(lowerBoundInput).toBeInTheDocument();
     expect(upperBoundInput).toBeInTheDocument();
@@ -86,19 +89,19 @@ describe('SearchLibrary Component', () => {
 
     expect(lowerBoundInput).toHaveValue(0);
     expect(upperBoundInput).toHaveValue(0);
-    expect(genreSelect).toHaveValue('');
+    expect(genreSelect).toHaveValue("");
 
-    const genreOptions = genreSelect.querySelectorAll('option');
+    const genreOptions = genreSelect.querySelectorAll("option");
     expect(genreOptions.length).toBe(17);
-    expect(genreOptions[0]).toHaveValue('');
-    expect(genreOptions[1]).toHaveValue('Blues');
+    expect(genreOptions[0]).toHaveValue("");
+    expect(genreOptions[1]).toHaveValue("Blues");
 
-    expect(screen.getByText('Lower Bound:')).toBeInTheDocument();
-    expect(screen.getByText('Upper Bound:')).toBeInTheDocument();
-    expect(screen.getByText('Genre:')).toBeInTheDocument();
-  })
+    expect(screen.getByText("Lower Bound:")).toBeInTheDocument();
+    expect(screen.getByText("Upper Bound:")).toBeInTheDocument();
+    expect(screen.getByText("Genre:")).toBeInTheDocument();
+  });
 
-  test('loads the next page (second page)', async () => {
+  test("loads the next page (second page)", async () => {
     global.fetch.mockResolvedValueOnce({
       json: async () => Promise.resolve(tracks_pagination),
     });
@@ -111,59 +114,16 @@ describe('SearchLibrary Component', () => {
       await new Promise((resolve) => setTimeout(resolve, 0));
     });
 
-    const nextPageButton = screen.getByText('Next Page');
-
-    global.fetch.mockResolvedValueOnce({
-      json: async () => Promise.resolve(tracks_pagination),
-    });
+    const nextPageButton = screen.getByText("Next Page");
 
     act(() => {
       fireEvent.click(nextPageButton);
     });
 
-    const secondPageTracks = await screen.findByText('Amoebiasis - Amoebic Ensemble');
-
+    await screen.findByText("Amoebiasis - Amoebic Ensemble");
   });
 
-  test('renders the component with loading state', () => {
-
-    act(() => {
-      render(<SearchLibrary />);
-    });
-
-    expect(screen.getByText('Loading...')).toBeInTheDocument();
-  });
-
-  test('renders the component with data', async () => {
-    const mockData = tracks;
-
-    global.fetch.mockResolvedValueOnce({
-      json: () => Promise.resolve(mockData),
-    });
-
-    act(() => {
-      render(<SearchLibrary />);
-    });
-
-    await act(async () => {
-      await new Promise((resolve) => setTimeout(resolve, 0));
-    });
-
-    expect(screen.queryByText('Loading...')).toBeNull();
-    expect(screen.getByText('Food - AWOL')).toBeInTheDocument();
-
-    const getSimilarButtons = screen.getAllByText('Get Similar Tracks');
-    expect(getSimilarButtons.length).toBe(15);
-
-    const getDetailsButtons = screen.getAllByText('See Details');
-    expect(getDetailsButtons.length).toBe(15);
-
-    const plusButtons = screen.getAllByText('+');
-    expect(plusButtons.length).toBe(15);
-
-  });
-
-  test('add first song in local storage playlist', async () => {
+  test("add first song in local storage playlist", async () => {
     const mockData = tracks;
 
     global.fetch.mockResolvedValueOnce({
@@ -173,32 +133,31 @@ describe('SearchLibrary Component', () => {
     act(async () => {
       const { container } = render(<SearchLibrary />);
 
-
       await act(async () => {
         await new Promise((resolve) => setTimeout(resolve, 0));
       });
 
-      const listItems = container.querySelectorAll('li');
+      const listItems = container.querySelectorAll("li");
 
       expect(listItems.length).toBeGreaterThan(0);
 
       const firstListItem = listItems[0];
 
-      const plusButton = firstListItem.getByText('+');
+      const plusButton = firstListItem.getByText("+");
       if (plusButton) {
         plusButton.click();
       }
 
-      const selectedTracks = JSON.parse(localStorage.getItem('selectedTracks')) || [];
+      const selectedTracks =
+        JSON.parse(localStorage.getItem("selectedTracks")) || [];
       const trackId = 0;
       const isTrackInLocalStorage = selectedTracks.includes(trackId);
 
       expect(isTrackInLocalStorage).toBe(true);
-
     });
-  })
+  });
 
-  test('remove first song in local storage playlist', async () => {
+  test("remove first song in local storage playlist", async () => {
     const mockData = tracks;
 
     global.fetch.mockResolvedValueOnce({
@@ -208,33 +167,31 @@ describe('SearchLibrary Component', () => {
     act(async () => {
       const { container } = render(<SearchLibrary />);
 
-
       await act(async () => {
         await new Promise((resolve) => setTimeout(resolve, 0));
       });
 
-      const listItems = container.querySelectorAll('li');
+      const listItems = container.querySelectorAll("li");
 
       expect(listItems.length).toBeGreaterThan(0);
 
       const firstListItem = listItems[0];
 
-      const plusButton = firstListItem.getByText('-');
+      const plusButton = firstListItem.getByText("-");
       if (plusButton) {
         plusButton.click();
       }
 
-      const selectedTracks = JSON.parse(localStorage.getItem('selectedTracks')) || [];
+      const selectedTracks =
+        JSON.parse(localStorage.getItem("selectedTracks")) || [];
       const trackId = 0;
       const isTrackInLocalStorage = selectedTracks.includes(trackId);
 
       expect(isTrackInLocalStorage).toBe(false);
-
     });
   });
 
-
-  test('should return specific data regarding values in filter', async () => {
+  test("should return specific data regarding values in filter", async () => {
     const mockData = tracks;
 
     global.fetch.mockResolvedValueOnce({
@@ -244,39 +201,39 @@ describe('SearchLibrary Component', () => {
     act(async () => {
       const { container } = render(<SearchLibrary />);
 
-
       await act(async () => {
         await new Promise((resolve) => setTimeout(resolve, 0));
       });
 
-  
-      const inputElements = screen.getAllByPlaceholderText('0');
+      const inputElements = screen.getAllByPlaceholderText("0");
       const lowerBoundInput = inputElements[0];
       const upperBoundInput = inputElements[1];
-  
+
       expect(lowerBoundInput).toBeInTheDocument();
-      expect(lowerBoundInput).toHaveValue('0');
+      expect(lowerBoundInput).toHaveValue("0");
 
       expect(upperBoundInput).toBeInTheDocument();
-      expect(upperBoundInput).toHaveValue('0');
-  
-      fireEvent.change(lowerBoundInput, { target: { value: '7' } });
-      fireEvent.change(upperBoundInput, { target: { value: '240' } });  
+      expect(upperBoundInput).toHaveValue("0");
 
-      const genereSelectElement = screen.getAllByPlaceholderText('---');
-      fireEvent.change(genereSelectElement, { target: { value: 'Country' } });
+      fireEvent.change(lowerBoundInput, { target: { value: "7" } });
+      fireEvent.change(upperBoundInput, { target: { value: "240" } });
 
-      const searchButton = screen.getByTestId('search-btn')
+      const genereSelectElement = screen.getAllByPlaceholderText("---");
+      fireEvent.change(genereSelectElement, { target: { value: "Country" } });
+
+      const searchButton = screen.getByTestId("search-btn");
       expect(searchButton).toBeInTheDocument();
 
-      const searchSpy = jest.spyOn(handleFormSubmit, 'handleFormSubmit').mockImplementation();
+      const searchSpy = jest
+        .spyOn(handleFormSubmit, "handleFormSubmit")
+        .mockImplementation();
 
       if (fireEvent.click(searchButton).valueOf()) {
         handleFormSubmitMock.handleFormSubmit();
         expect(searchSpy).toHaveBeenCalled();
       }
 
-      const songTitle = screen.getByText('Notturno')
+      const songTitle = screen.getByText("Notturno");
       expect(songTitle).toBeInTheDocument();
     });
   });
